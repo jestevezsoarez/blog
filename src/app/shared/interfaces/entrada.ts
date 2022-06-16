2b0e2d3864e5eaaf5fb2d063284df3c1a03d6914
Javier Estevez <javierdaniloestevez@gmail.com>
2022-06-16 02:40:00 -0300

    agrego interface de entrada y algunas entradas al componente listado

---
+++ src/app/shared/interfaces/entrada.ts
@@ -0,0 +1,5 @@
+export interface Entrada {
+  titulo      : string;
+  ingredientes: string;
+  preparacion : string;
+}
--- src/app/views/listado/listado.component.ts
+++ src/app/views/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Entrada } from 'src/app/shared/interfaces/entrada';
 
 @Component({
   selector: 'app-listado',
@@ -6,8 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent implements OnInit {
+  public listadoEntradas: Entrada[];
 
-  constructor() { }
+  constructor() {
+    this.listadoEntradas = [
+      {
+        titulo: 'Pizza casera',
+        ingredientes: `1 kilogramo de harina de fuerza (también conocida como harina 00)
+        1 cucharadita de sal fina
+        2½ tazas de agua tibia
+        2 cucharadas soperas de aceite de oliva
+        30 gramos de levadura fresca`,
+        preparacion: `Mezclar en un bowl y amasar. Dejar reposar por 1 hora. Luego colocar la salsa y muzzarella y llevar al horno`
+      },
+      {
+        titulo: 'Helado de Frutillas',
+        ingredientes: `500 ml de nata para montar
+        100 g de azúcar glas
+        370 g de leche condensada
+        1 Kg de frutillas`,
+        preparacion: 'Cortar las frutillas en trocitos, mezclar todo y poner a congelar.'
+      },
+      {
+        titulo: 'Ravioles con tuco',
+        ingredientes: `1 litro de agua hirviendo
+        1/2 Kilo de Ravioles
+        Tuco a gusto
+        Queso rallado a gusto`,
+        preparacion: 'En el agua hirviendo agregar los ravioles y dejarlos hervir hasta que esten prontos.Colar y ponerles tuco y queso a gusto'
+      }
+
+    ]
+   }
 
   ngOnInit(): void {
   }
